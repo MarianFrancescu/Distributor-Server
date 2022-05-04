d87587d68f0d809c963b264dcb93ddcc16d52ab9
Marian Francescu <marian.francescu98@gmail.com>
2022-05-04 17:23:45 +0300

    Integrated OAuth2 for gmail service

--- src/services/EmailService.js
+++ src/services/EmailService.js
@@ -1,13 +1,39 @@
 require('dotenv').config();
+const { google } = require("googleapis");
 const nodemailer = require("nodemailer");
+const OAuth2 = google.auth.OAuth2;
+
+const oauth2Client = new OAuth2(
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SECRET,
+    "https://developers.google.com/oauthplayground"
+);
+
+oauth2Client.setCredentials({
+    refresh_token: process.env.REFRESH_TOKEN
+});
+
 
 const sendCredentialEmail = async (name, email) => {
+    const accessToken = await new Promise((resolve, reject) => {
+        oauth2Client.getAccessToken((err, token) => {
+          if (err) {
+            reject("Failed to create access token :(");
+          }
+          resolve(token);
+        });
+      });
+
     const transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
+            type: "OAuth2",
             user: process.env.EMAIL,
-            pass: process.env.PASSWORD
-        }
+            accessToken,
+            clientId: process.env.CLIENT_ID,
+            clientSecret: process.env.CLIENT_SECRET,
+            refreshToken: process.env.REFRESH_TOKEN
+          }
     });
 
     const mailOptions = {
@@ -27,12 +53,25 @@ const sendCredentialEmail = async (name, email) => {
 }
 
 const sendPasswordEmail = async (password, email) => {
+    const accessToken = await new Promise((resolve, reject) => {
+        oauth2Client.getAccessToken((err, token) => {
+          if (err) {
+            reject("Failed to create access token :(");
+          }
+          resolve(token);
+        });
+      });
+      
     const transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
+            type: "OAuth2",
             user: process.env.EMAIL,
-            pass: process.env.PASSWORD
-        }
+            accessToken,
+            clientId: process.env.CLIENT_ID,
+            clientSecret: process.env.CLIENT_SECRET,
+            refreshToken: process.env.REFRESH_TOKEN
+          }
     });
 
     const mailOptions = {
